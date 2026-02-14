02c2a93ab5f39e41c4d6780bc43afffc0164a7db
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-02-14 15:05:07 -0500

    feat: code editor layout

---
+++ client/components/editor/index.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { X } from "lucide-react"
+import { Button } from "../ui/button"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "../ui/resizable"
+import { Editor, OnMount } from "@monaco-editor/react"
+import { useRef } from "react"
+import monaco from "monaco-editor"
+
+const CodeEditor = () => {
+    const editorRef = useRef<null | monaco.editor.IStandaloneCodeEditor>(null);
+
+    const handleEditorMount: OnMount = (editor, monaco) => {
+        editorRef.current = editor;
+    }
+
+    return (
+        <>
+        <div className="h-full w-52"></div>
+        <ResizablePanelGroup orientation="horizontal">
+            <ResizablePanel minSize={30} defaultSize={60} className="flex flex-col p-2">
+                <div className="h-10 w-full flex gap-2">
+                    <Button variant={"secondary"} size={"sm"} className="min-w-20 justify-between">
+                        index.html <X className="w-3 h-3" />
+                    </Button>
+                    <Button variant={"secondary"} size={"sm"} className="min-w-20 justify-between">
+                        style.css <X className="w-3 h-3" />
+                    </Button>
+                </div>
+                <div className="grow w-full overflow-hidden rounded-lg relative">
+                    <Editor height={"100%"} defaultLanguage="typescript" theme="vs-dark" onMount={handleEditorMount}/>
+                </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={40}>
+                <ResizablePanelGroup orientation="vertical" >
+                    <ResizablePanel defaultSize={40} minSize={25} className="p-2 flex flex-col">
+                        <div className="h-10 w-full flex gap-2">
+                            <Button variant={"secondary"} size={"sm"} className="min-w-20 justify-between">
+                                localhost:3000 <X className="w-3 h-3" />
+                            </Button>
+                        </div>
+                        <div className="w-full grow rounded-lg bg-foreground"></div>
+                    </ResizablePanel>
+                    <ResizableHandle />
+                    <ResizablePanel defaultSize={50} minSize={20} className="p-2 flex flex-col">
+                        <div className="h-10 w-full flex gap-2">
+                            <Button variant={"secondary"} size={"sm"} className="min-w-20 justify-between">
+                                Node <X className="w-3 h-3" />
+                            </Button>
+                        </div>
+                        <div className="w-full grow rounded-lg bg-foreground"></div>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+        </>
+    )
+}
+
+export default CodeEditor
