3bf8dcce595ea42bf2b17818019546130be91ecf
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-02-14 17:15:35 -0500

    feat: home page

--- client/components/navbar/index.tsx
+++ client/components/navbar/index.tsx
@@ -2,14 +2,16 @@ import { Pencil } from "lucide-react"
 import Image from "next/image"
 import Logo from "@/assets/logo.svg"
 import { UserButton } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Link from "next/link"
 
 const Navbar = () => {
     return (
         <div className="h-14 px-2 w-full border-b border-border flex items-center justify-between">
             <div className="flex items-center space-x-4">
-                <button className="ring-offset-2 ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none rounded-sm">
+                <Link href={"/"} className="ring-offset-2 ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none rounded-sm">
                     <Image src={Logo} alt="Logo" width={36} height={36} />
-                </button>
+                </Link>
                 <div className="flex items-center text-sm font-medium">
                     My React Project {" "}
                     <div className="h-7 w-7 flex ml-1 items-center justify-center transition-colors bg-transparent hover:bg-muted-foreground/25 rounded-md">
@@ -17,7 +19,9 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            <UserButton />
+            <UserButton appearance={{
+                baseTheme: dark
+            }} />
         </div>
     )
 }
