b9472283eefa4a862ccce0e5e1c290566d0821f4
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-02-15 14:06:55 -0500

    feat: user route

--- server/database/src/index.ts
+++ server/database/src/index.ts
@@ -11,12 +11,36 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { eq } from "drizzle-orm";
+import { user } from "./schema";
+import { json } from "itty-router-extras";
+import { drizzle } from "drizzle-orm/d1";
+
 export interface Env {
 	DB: D1Database;
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		const path = url.pathname;
+		const method = request.method;
+
+		const db = drizzle(env.DB);
+
+		if (path === "/api/user" && method === "GET") {
+			const params = url.searchParams;
+
+			if (params.has("id")) {
+				const id = params.get("id") as string;
+				const res = await db.select().from(user).where(eq(user.id, id)).get();
+				return json(res ?? {});
+			} else {
+				const res = await db.select().from(user).all();
+				return new Response(JSON.stringify(res));
+			}
+		} else {
+			return new Response("Not Found", {status: 404})
+		}
 	},
 } satisfies ExportedHandler<Env>;
